63c6fda9fccf7712bc80a8a5107aaf876897a21d
Marko <marko.sestanovic1@gmail.com>
2026-04-08 13:51:42 +0200

    typo

--- v2/env/dev/compute/outputs.tf
+++ v2/env/dev/compute/outputs.tf
@@ -8,7 +8,7 @@ output "vpn_download_command" {
     value       = module.compute.vpn_download_command
 }
 
-output "vpn_download_command" {
+output "alb_arn" {
     description = "Download command for the VPN connection"
     value       = module.compute.alb_arn
 }
