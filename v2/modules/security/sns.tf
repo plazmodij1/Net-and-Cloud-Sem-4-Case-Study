2daa8a92652a465f1fdc4f7b88a4f9e2cbb880a1
Marko <marko.sestanovic1@gmail.com>
2026-04-09 11:02:41 +0200

    testing email

--- v2/modules/security/sns.tf
+++ v2/modules/security/sns.tf
@@ -2,7 +2,7 @@ resource "aws_sns_topic" "waf_alerts" {
     name = "${var.env}-waf-spam-alerts"
 }
 
-resource "aws_sns_topic_subscription" "email" {
+resource "aws_sns_topic_subscription" "email_v2" {
     topic_arn = aws_sns_topic.waf_alerts.arn
     protocol = "email"
     endpoint = var.email
