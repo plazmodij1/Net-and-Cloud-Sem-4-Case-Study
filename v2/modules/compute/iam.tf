d4e04bc2ed91e3fad76b60ddde714af39ddf796e
Marko <i558991@fontysict.nl>
2026-05-18 16:18:41 +0200

    fixed a typo in iam.tf in compute module

--- v2/modules/compute/iam.tf
+++ v2/modules/compute/iam.tf
@@ -78,7 +78,7 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
 
 #OIDC Provider
 resource "aws_iam_openid_connect_provider" "eks" {
-    client_id_list = [sts.amazonaws.com]
+    client_id_list = ["sts.amazonaws.com"]
     thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
     url = aws_eks_cluster.main.identity[0].oidc[0].issuer
 
