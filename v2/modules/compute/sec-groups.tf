4ea62447f547911cf390c32b15af962ad3b54e05
Marko <i558991@fontysict.nl>
2026-05-24 17:31:23 +0200

    fixed a typo

--- v2/modules/compute/sec-groups.tf
+++ v2/modules/compute/sec-groups.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "alb" {
   ingress {
     description = "Allow HTTPS traffic"
     from_port = 443
-    to_port = 80
+    to_port = 443
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
