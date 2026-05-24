621261111b47bc8d8b95f7d2bba8c758fd400ea3
Marko <i558991@fontysict.nl>
2026-05-24 17:27:00 +0200

    added sg rule for alb to allow port 443 traffic

--- v2/modules/compute/sec-groups.tf
+++ v2/modules/compute/sec-groups.tf
@@ -10,6 +10,13 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress = {
+    description = "Allow HTTPS traffic"
+    from_port = 443
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
