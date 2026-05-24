f6deeb1e37b6da7b35d8a0eb18a73625c28039e1
Marko <i558991@fontysict.nl>
2026-05-24 14:49:36 +0200

    fixed outputs.tf for cicd runner

--- v2/env/dev/compute/outputs.tf
+++ v2/env/dev/compute/outputs.tf
@@ -16,3 +16,8 @@ output "alb_arn" {
 output "ecs_portal_sg_id" {
     value = module.compute.ecs_portal_sg_id
     }
+
+output "eks-portal-subnets" {
+    description = "Private subnets passed through from the networking layer"
+    value = data.terraform_remote_state.networking.outputs.eks-portal-subnets
+}
