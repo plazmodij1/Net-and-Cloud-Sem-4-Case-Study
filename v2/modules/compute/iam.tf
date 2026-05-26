de2348fa2b077a0bd97ba48c5cdaf12b8298573e
Marko <marko.sestanovic1@gmail.com>
2026-05-26 11:55:21 +0200

    typo

--- v2/modules/compute/iam.tf
+++ v2/modules/compute/iam.tf
@@ -162,7 +162,7 @@ resource "aws_iam_policy" "ecs_ssm_read" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_ssm_attach" {
-  role = aws_iam_role.ecs-task-role.name
+  role = aws_iam_role.ecs_task_role.name
   policy_arn = aws_iam_policy.ecs_ssm_read.arn
 }
 
